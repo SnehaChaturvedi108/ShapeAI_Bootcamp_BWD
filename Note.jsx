b83c0719aea8e1d004cb9078863576d80f6da48c
SnehaChaturvedi108 <83336025+SnehaChaturvedi108@users.noreply.github.com>
2021-06-05 15:27:42 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+function Note()
+{
+  return(
+    <div className="note">
+      <h1>
+        Javascript and react.js
+        </h1>
+        
+    <p>
+      This was taken by shaurya shinha
+       sir we covered everything from 
+       srcatch including javascript
+        and react.js , html.</p>
+        </div>
+        );
+}
+export default Note;
